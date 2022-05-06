3b47b31fde78e6d0f0eca3d61613b6a91e3d3b15
Bruno Alves <78178727+Bruno99Alves@users.noreply.github.com>
2022-05-06 10:29:14 +0100

    otimConvNext  fixed

--- Scripts/otimConvNext.py
+++ Scripts/otimConvNext.py
@@ -15,6 +15,7 @@ with open(fich,"w") as o:
     
     for otim in otimList:
         for lrexp in lrexpList:
-            comd = "python main.py --model convnext_base --drop_path 0.8 --input_size 224 --batch_size 32 --opt {} --lr 5e-{} --update_freq 2 --warmup_epochs 0 --epochs {} --weight_decay 1e-8  --layer_decay 0.7 --head_init_scale 0.001 --cutmix 0 --mixup 0 --finetune ../Checkpoints/convnext_base_22k_224.pth --data_path ../Datasets/padded --output_dir ../Results\n\n\n".format(otim,lrexp,n)
+            o.write("mkdir {}_{}".format(otim,lrexp))
+            comd = "python main.py --model convnext_base --drop_path 0.8 --input_size 224 --batch_size 32 --opt {} --lr 5e-{} --min_lr 1e-{} --update_freq 2 --warmup_epochs 0 --epochs {} --weight_decay 1e-8  --layer_decay 0.7 --head_init_scale 0.001 --cutmix 0 --mixup 0 --finetune ../Checkpoints/convnext_base_22k_224.pth --data_path ../Datasets/padded --nb_classes 196 --output_dir ../Results/OtimConvNext/{}_{}\n\n\n".format(otim,lrexp,lrexp+1,n,otim,lrexp)
             o.write(comd)
         
